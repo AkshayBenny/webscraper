f966b05ce9222210578dd8db0347572a2a361db7
AkshayBenny <akshaybennygh@gmail.com>
2021-10-10 15:17:17 +0530

    update

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import './App.css';
-import Header from './components/Header/Header'
-import Home from './components/Home/Home'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Checkout from "./components/Checkout/Checkout";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
