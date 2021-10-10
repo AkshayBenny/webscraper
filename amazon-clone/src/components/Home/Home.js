cdb362c87ffb80b5fc6e485fe63e94d832626a26
AkshayBenny <akshaybennygh@gmail.com>
2021-10-10 09:48:39 +0530

    update

--- amazon-clone/src/components/Home/Home.js
+++ amazon-clone/src/components/Home/Home.js
@@ -1,25 +1,32 @@
-import React from 'react'
-import './Home.css'
-import Product from './Product/Product'
+import React from "react";
+import "./Home.css";
+import Product from "./Product/Product";
 
 function Home() {
-    return (
-        <div className="home">
-           <div className="home-top">
-            <img className="home-top-bkg" src="https://www.orissapost.com/wp-content/uploads/2019/09/Top-smartphone-deals-during-Big-Billion-Days-Great-Indian-Festival-sales.jpeg" alt="background image"></img>
-           </div>
+  return (
+    <div className="home">
+      <div className="home-top">
+        <img
+          className="home-top-bkg"
+          src="https://www.orissapost.com/wp-content/uploads/2019/09/Top-smartphone-deals-during-Big-Billion-Days-Great-Indian-Festival-sales.jpeg"
+          alt="background"
+        ></img>
+      </div>
 
-           <div className="rows">
-                <div className="row-one row">
-                    <Product />
-                    <Product />
-                    <Product />
-                    <Product />
-                </div>
-           </div>
-           
+      <div className="rows">
+        <div className="row-one row">
+          <Product
+            title="A song of Ice and fire"
+            price={20.55}
+            rating={4}
+            img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSjwzQKRhNCGWqxLuMYR_Q34oW7NeqXT7vHA&usqp=CAU"
+          />
+
+          <Product />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- amazon-clone/src/components/Home/Product/Product.js
+++ amazon-clone/src/components/Home/Product/Product.js
@@ -1,31 +1,36 @@
-import React from 'react'
-import './Product.css'
+import React from "react";
+import "./Product.css";
 
-function Product() {
-    return (
-        <div className="product">
-            <div className="title-cont elem">
-                <p className="title">From Goethe to Gundolf by Roger Paulin</p>
-            </div>
+function Product({ title, price, rating, img }) {
+  return (
+    <div className="product">
+      <div className="title-cont elem">
+        <p className="title">{title}</p>
+      </div>
 
-            <div className="price-cont elem">
-                <p className="price">$ 119.9</p>
-            </div>
+      <div className="price-cont elem">
+        <p className="price">$ {price}</p>
+      </div>
 
-            <div className="rating-cont elem">
-              <p className="rating">⭐⭐⭐⭐⭐</p>  
-            </div>
+      <div className="rating-cont elem">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <p className="rating">⭐</p>
+          ))}
+      </div>
 
-            <div className="pdt-image-cont elem">
-                <img className="pdt-img" src="https://www.openbookpublishers.com/shopimages/products/thumbnails/1425.jpg"></img>
-            </div> 
+      <div className="pdt-image-cont elem">
+        <img className="pdt-img" src={img} alt="img"></img>
+      </div>
 
-            <div className="btn-cont elem">
-                <button type="submit" className="add-to-cart-btn">Add to cart</button>
-            </div>
-            
-        </div>
-    )
+      <div className="btn-cont elem">
+        <button type="submit" className="add-to-cart-btn">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
