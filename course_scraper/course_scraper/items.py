3a247b8e18cc528995166df332992c7dc37c16ae
Akshay <akshaybennygh@gmail.com>
2023-02-01 19:24:15 +0530

    edit

--- course_scraper/course_scraper/items.py
+++ course_scraper/course_scraper/items.py
@@ -1,12 +1,11 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy import Item, Field
 
 
-class CourseScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class CourseScraperItem(Item):
+    #    
+    pass
+
+class EbookScraperItem(Item):
+    title = Field()
+    price = Field()
     pass
