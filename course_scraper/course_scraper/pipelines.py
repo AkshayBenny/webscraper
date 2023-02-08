751d59fdcd18561a9d5f84bb6fef64df2ddfae21
Akshay <akshaybennygh@gmail.com>
2023-02-08 18:14:42 +0530

    mongo pipeline setup

--- course_scraper/course_scraper/pipelines.py
+++ course_scraper/course_scraper/pipelines.py
@@ -1,20 +1,28 @@
-from openpyxl import Workbook
+from pymongo import MongoClient
 from itemadapter import ItemAdapter
 
 
 class EbookScraperPipeline:
     # here spider is the instance of the currencly running spider
     def open_spider(self,spider):
-        self.workbook = Workbook()
-        self.sheet = self.workbook.active
-        self.sheet.title = 'Ebooks' #used to set the title of the sheet in excel file
-        # self.sheet.append(['Title','Price']) OR
-        self.sheet.append(spider.cols)
+        self.client = MongoClient(
+            host="mongodb+srv://admin:admin@cluster0.rsqjb.mongodb.net/?retryWrites=true&w=majority",
+            connect=False
+        )
+        self.collection = self.client.get_database('scraper').get_collection('ebooks')
 
 
 
     def process_item(self, item, spider):
-        self.sheet.append([item['title'],item['price']])
+        # self.collection.insert_one({
+        #     'title': item['title'],
+        #     'price': item['price']
+        # })
+        # OR
+        self.collection.insert_one(ItemAdapter(item).asdict())
 
     def close_spider(self,spider):
-        self.workbook.save('ebooks.xlsx')
+        self.client.close()
+
+
+    # mongodb+srv://admin:<password>@cluster0.rsqjb.mongodb.net/?retryWrites=true&w=majority
