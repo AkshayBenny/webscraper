2440fc6bef3306a0a1205b20e14f1ec627dcc520
Akshay <akshaybennygh@gmail.com>
2023-02-08 18:59:17 +0530

    pagination follow

--- course_scraper/course_scraper/spiders/ebook.py
+++ course_scraper/course_scraper/spiders/ebook.py
@@ -1,4 +1,4 @@
-from scrapy import Spider
+from scrapy import Spider, Request
 # from ebook_scraper.item import EbookScraperItem 
 # OR
 from ..items import EbookScraperItem
@@ -9,12 +9,17 @@ def get_price_as_float(price):
 
 class EbookSpider(Spider):
     name="ebook"
-    start_urls = ['https://books.toscrape.com']
+    start_urls = ['https://books.toscrape.com/catalogue/category/books/sequential-art_5/']
+
+    def __init__(self):
+        super().__init__()
+        self.page_count = 0
+
     cols = ['title','price']
     def parse(self, response):
         print('---------------------------------------------')
         ebooks = response.css('article')
-        
+        self.page_count += 1
         for ebook in ebooks:
             loader = ItemLoader(item=EbookScraperItem(), selector=ebook)
             # loader.add_value('title',ebook.css('h3 a::attr(title)').get())
@@ -22,4 +27,14 @@ class EbookSpider(Spider):
             loader.add_value('price', ebook.css('p.price_color::text').get())
 
             yield loader.load_item()
+        print('Page Count: >>>>>>>>>>>>>>>>>>>>>>>>>>.', self.page_count)
+        next_btn = response.css('li.next a').get()
+       
+        if(next_btn):
+            next_url = response.css('li.next a::attr(href)').get()
+            next_page_complete_url = self.start_urls[0] + '/' + next_url
+            yield Request(url=next_page_complete_url)
+       
+
+      
         
